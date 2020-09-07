760941a5f74263ffb1fc41115e275ab3a6768adc
ranjian0 <karanjaichungwa@gmail.com>
2020-09-07 21:13:48 +0300

    use cmake for building

--- setup.py
+++ setup.py
@@ -1,62 +1,65 @@
 import os
-import conf
+import re
+import sys
 import pathlib
-import setuptools
-from sys import platform as _platform
-from distutils.core import setup, Extension
+import platform
+import subprocess
 
-# Enable ccache to speed up builds (Development)
-# ON_LINUX = "linux" in _platform
-# if ON_LINUX:
-#     os.environ['CC'] = 'ccache gcc'
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
 
 
-# monkey-patch for parallel compilation
-import multiprocessing
-import multiprocessing.pool
+class CMakeExtension(Extension):
+
+    def __init__(self, name, sourcedir=''):
+        # don't invoke the original build_ext for this special extension
+        super().__init__(name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
 
 
-def parallelCCompile(self, sources, output_dir=None, macros=None, include_dirs=None,
-                     debug=0, extra_preargs=None, extra_postargs=None, depends=None):
+class CMakeBuild(build_ext):
 
-    # those lines are copied from distutils.ccompiler.CCompiler directly
-    macros, objects, extra_postargs, pp_opts, build = self._setup_compile(
-      output_dir, macros, include_dirs, sources, depends, extra_postargs)
-    cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
-    # parallel code
-    N = 2 * multiprocessing.cpu_count()  # number of parallel compilations
-    try:
-        # On Unix-like platforms attempt to obtain the total memory in the
-        # machine and limit the number of parallel jobs to the number of Gbs
-        # of RAM (to avoid killing smaller platforms like the Pi)
-        mem = os.sysconf('SC_PHYS_PAGES') * os.sysconf('SC_PAGE_SIZE')  # bytes
-    except (AttributeError, ValueError):
-        # Couldn't query RAM; don't limit parallelism (it's probably a well
-        # equipped Windows / Mac OS X box)
-        pass
-    else:
-        mem = max(1, int(round(mem / 1024**3)))  # convert to Gb
-        N = min(mem, N)
-
-    def _single_compile(obj):
+    def run(self):
         try:
-            src, ext = build[obj]
-        except KeyError:
-            return
-        newcc_args = cc_args
-        if _platform == "darwin":
-            if src.endswith('.cpp'):
-                newcc_args = cc_args + ["-mmacosx-version-min=10.7", "-stdlib=libc++", "-std=c++11"]
-        self._compile(obj, src, ext, newcc_args, extra_postargs, pp_opts)
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
 
-    # convert to list, imap is evaluated on-demand
-    pool = multiprocessing.pool.ThreadPool(N)
-    list(pool.imap(_single_compile, objects))
-    return objects
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
 
+        for ext in self.extensions:
+            self.build_extension(ext)
+        super().run()
 
-import distutils.ccompiler
-distutils.ccompiler.CCompiler.compile = parallelCCompile
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j3']
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
 
 # The directory containing this file
@@ -64,28 +67,25 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE/"README.md").read_text()
 
 
-def main():
-    setup(name="assimp_py",
-          version="1.0.2",
-          long_description=README,
-          long_description_content_type="text/markdown",
-          description="Minimal Python Bindings for ASSIMP Library using C-API",
-          author="Ian Ichung'wah",
-          author_email="karanjaichungwa@gmail.com",
-          license="MIT",
-          classifiers=[
-              "License :: OSI Approved :: MIT License",
-              "Programming Language :: Python :: 3",
-              "Programming Language :: Python :: 3.6"
-              "Programming Language :: Python :: 3.7",
-              "Programming Language :: Python :: 3.8"
-          ],
-          packages=["assimp_py"],
-          ext_modules=[Extension("assimp_py.assimp_py",
-                       sources=conf.ASSIMP_SOURCES + ["assimp_py/assimp_py.c"],
-                       define_macros=conf.ASSIMP_DEFINES,
-                       include_dirs=conf.ASSIMP_INCLUDE_DIRS)])
-
-
-if __name__ == "__main__":
-    main()
+setup(
+    name="assimp_py",
+    version="1.0.2",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    description="Minimal Python Bindings for ASSIMP Library using C-API",
+    author="Ian Ichung'wah",
+    author_email="karanjaichungwa@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
+    ],
+    packages=["assimp_py"],
+    ext_modules=[CMakeExtension("assimp_py.assimp_py")],
+    cmdclass={
+        'build_ext': CMakeBuild,
+    }
+)
